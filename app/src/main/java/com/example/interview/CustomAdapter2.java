b99654770035f0e177964e50fc976252d6c5b12e
chinococo <garyliuyahoocom384@gmail.com>
2021-11-07 22:02:00 +0800

    update

--- app/src/main/java/com/example/interview/CustomAdapter2.java
+++ app/src/main/java/com/example/interview/CustomAdapter2.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.HashMap;
@@ -14,6 +16,7 @@ public class CustomAdapter2 extends ArrayAdapter {
     List<HashMap<String,String>> arrayList;
     Context context;
     private int resourceId;
+    ImageView plus,minus;
 
     public CustomAdapter2(Context context1, int textViewResourceId, List<HashMap<String,String>> objects){
         super(context1,textViewResourceId,objects);
@@ -30,10 +33,28 @@ public class CustomAdapter2 extends ArrayAdapter {
         //ImageView Image = (ImageView)view.findViewById(R.id.fruit_image);
         TextView name=(TextView) view.findViewById(R.id.ItemName);
         TextView price=(TextView) view.findViewById(R.id.ItemPrice);
+        TextView now_count;
+        plus = view.findViewById(R.id.imageplus);
+        minus = view.findViewById(R.id.imageminus);
+        now_count = view.findViewById(R.id.count);
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                now_count.setText(""+(Integer.parseInt(now_count.getText().toString())+1));
+            }
+        });
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Integer.parseInt(now_count.getText().toString())>0)
+                now_count.setText(""+(Integer.parseInt(now_count.getText().toString())-1));
+            }
+        });
         //Image.setImageResource(fruit.getImageId());
         System.out.println(data.toString());
         name.setText("商品:"+data.get("itemName"));
         price.setText("價格:"+data.get("itemPrice"));
         return view;
     }
+
 }
--- app/src/main/java/com/example/interview/order.java
+++ app/src/main/java/com/example/interview/order.java
@@ -1,5 +1,6 @@
 package com.example.interview;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -7,11 +8,15 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -29,7 +34,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class order extends AppCompatActivity {
+    public ArrayList<HashMap<String,String>> all_order = new ArrayList<>();
     ListView listview;
+    int sum;
+    Button button;
+    ArrayList<HashMap<String,String>> submitdata;
     DatabaseReference db = FirebaseDatabase.getInstance().getReference();
     ArrayList<HashMap<String,String>> data;
     @Override
@@ -38,7 +47,6 @@ public class order extends AppCompatActivity {
         Intent intent = getIntent();
         Log.e("now",intent.getExtras().getString("restuarantName"));
         listview = findViewById(R.id.goodListview);
-
         db.child("menu").child(intent.getExtras().getString("restuarantName")).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -56,8 +64,66 @@ public class order extends AppCompatActivity {
 
             }
         });
+        button = findViewById(R.id.sumup);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sum=0;
+                String data="";
+                int itemsCount = listview.getChildCount();
+                submitdata = new ArrayList<HashMap<String,String>>();
+                for (int i = 0; i < itemsCount; i++) {
+                    View view = listview.getChildAt(i);
+                    String good = ((TextView) view.findViewById(R.id.ItemName)).getText().toString();
+                    String price = ((TextView) view.findViewById(R.id.ItemPrice)).getText().toString();
+                    String count = ((TextView) view.findViewById(R.id.count)).getText().toString();
+                    HashMap<String,String> temp = new HashMap<>();
+                    temp.put("GoodName",good.substring(3));
+                    temp.put("price",price.substring(3));
+                    temp.put("count",count);
+                    if(Integer.parseInt(count)!=0) {
+                        submitdata.add(temp);
+                        sum+=Integer.parseInt(price.substring(3)) * Integer.parseInt(count);
+                        data += good + " " + price + " * " + count + " = " + Integer.parseInt(price.substring(3)) * Integer.parseInt(count) + "\n";
+                    }
+                }
+                data+="總計:"+sum;
+                new AlertDialog.Builder(order.this)
+                        .setTitle("是否送出")
+                        .setMessage(data)
+                        .setCancelable(false)
+                        .setNegativeButton("否", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                .setPositiveButton("送出", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+                        reference.child("Task").child(intent.getExtras().getString("restuarantName")).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                ArrayList<ArrayList<HashMap<String,String>>> t = new ArrayList<>();
+                                if(snapshot.getValue()!=null)
+                                {
+                                    t = (ArrayList<ArrayList<HashMap<String, String>>>) snapshot.getValue();
+                                }
+                                t.add(submitdata);
+                                reference.child("Task").child(intent.getExtras().getString("restuarantName")).setValue(t);
+                            }
 
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
 
+                            }
+                        });
+                    }
+                }).show();
+                Log.e("sumup",data);
+            }
+        });
 
         super.onCreate(savedInstanceState);
 
